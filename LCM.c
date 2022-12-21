75463adfc4dfffb6f2ed51da78c4886c7db49926
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2022-12-21 21:17:02 +0530

    #include<stdio.h> int main(){     int a,b,i,g=1,l;     scanf("%d%d",&a,&b);     for(i=1;i<=a && i<=b;i++){         if(a%i==0 && b%i==0){             g=i;         }     }     l=a*b/g;     printf("%d",l); }

---
+++ LCM.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,i,g=1,l;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=a && i<=b;i++){
+        if(a%i==0 && b%i==0){
+            g=i;
+        }
+    }
+    l=a*b/g;
+    printf("%d",l);
+}
