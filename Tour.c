e1730c9b7720e423654d627642addd2c218d04ba
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2022-12-29 21:25:17 +0530

    #include<stdio.h> int main(){     int n,x,y,i;     scanf("%d",&n);
        for(i=0;i<n;i++){
            scanf("%d%d",&x,&y);
            printf("%d
    ",5*x+7*y);
        }
        
    }

---
+++ Tour.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,x,y,i;
+    scanf("%d",&n);
+    
+        
+    for(i=0;i<n;i++){
+        scanf("%d%d",&x,&y);
+        printf("%d
+",5*x+7*y);
+    }
+    
+}
