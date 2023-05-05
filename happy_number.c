6fe9ca1b1cb53e1760815655d5d9e7aaa4416793
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-05 22:08:58 +0530

    #include<stdio.h> #include<stdlib.h> #include<math.h> int ss(int n) { if(n==0)  return 0; return ((n%10)*(n%10)+ss(n/10)); } int h(int n) { if(n==1)  return 1;  if(n==4) return 0; return h(ss(n)); } int main() {  int n; scanf("%d",&n);  if(h(n)==1)  { printf("True"); }  else  {  printf("False"); } }

--- happy_number.c
+++ happy_number.c
@@ -1,23 +1,30 @@
-#include<stdio.h> 
-#include<math.h> 
-main() 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int ss(int n)
 { 
- int i,j,num,sum=0; 
- scanf("%d",&num); 
-  while(sum!=1 && sum!=4) 
-  { 
-   sum=0; 
-   while(num>0) 
-  { 
-    j=num%10; 
-    sum+=(j*j); 
-    num=num/10;  
-  } 
-  num=sum; 
-  } 
-   
-  if(sum==1) 
- printf("True"); 
- else 
+if(n==0)
+ return 0; 
+return ((n%10)*(n%10)+ss(n/10));
+}
+int h(int n)
+{ 
+if(n==1)
+ return 1;
+ if(n==4) 
+return 0; 
+return h(ss(n));
+}
+int main()
+{
+ int n; 
+scanf("%d",&n);
+ if(h(n)==1)
+ { 
+printf("True"); 
+}
+ else
+ {
  printf("False"); 
-} 
+}
+}
