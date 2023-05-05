576dfbf9e9e1cb9afad1cd28eb6df0db0fab7f32
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-05 22:59:45 +0530

    #include<stdio.h> int main() {  int a; scanf("%d",&a);  int arr[a],i,j,k,l=1,v=0,c=0; for (i=0; i<a; i++) { scanf("%d",&arr[i]); }  k=arr[0]; for (i=1; i<a; i++) {  if (arr[i]==k) {  l++;  }  } if (l==a) { printf("%d",0);  } else { for (i=0; i<a; i++) { c=0;  for (j=i; j<a; j++) { if (arr[i]==arr[j]) {  c++;  if (c>v) {  v=c; } } }  }  printf("%d",v); } return 0; }

--- Make_Array_Elements_Equal.c
+++ Make_Array_Elements_Equal.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 int main()
-{ 
-int a; 
-scanf("%d",&a); 
-int arr[a],i,j,k,l=1,v=0,c=0; 
+{
+ int a; 
+scanf("%d",&a);
+ int arr[a],i,j,k,l=1,v=0,c=0; 
 for (i=0; i<a; i++)
 { 
-scanf("%d",&arr[i]);
- } 
-k=arr[0]; 
+scanf("%d",&arr[i]); 
+}
+ k=arr[0]; 
 for (i=1; i<a; i++)
-{ 
-if (arr[i]==k)
-{ 
-l++;
+{
+ if (arr[i]==k)
+{
+ l++;
  }
  } 
 if (l==a)
 { 
-printf("%d",0); 
-}
- else
+printf("%d",0);
+ } 
+else
 { 
 for (i=0; i<a; i++)
 { 
@@ -28,15 +28,15 @@ c=0;
  for (j=i; j<a; j++)
 { 
 if (arr[i]==arr[j])
-{ 
-c++; 
-if (c>v)
-{ 
-v=c; 
-}
- } 
+{
+ c++;
+ if (c>v)
+{
+ v=c; 
+} 
 } 
 }
+ }
  printf("%d",v); 
 } 
 return 0;
