33be001f8437af9abcf7f5cab34b5b143850d14c
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-05-20 10:28:33 +0530

    #include<stdio.h> int prime(int n) {     int c=0,flag=0,i;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c=c+1;         }     }     if(c==2)     {         flag=1;     }     else     {         flag=0;     }     return flag; } int main() {     int n,i,j,p1,p2,c;     scanf("%d",&n);     for(i=2;i<=10000;i++)     {         for(j=2;j<=10000;j++)         {             if(i!=j && i*j==n)             {                 if(prime(i)==1 && prime(j)==1)                 {
                        p1=i;
                        p2=j;
                        c=1;
                        break;
                        
                    
                }
            }
        }
        }
        if(c==1)
        {
            printf("%d %d",p2,p1);
        }
        else
        {
            printf("-1");
        }
        
    }

---
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int c=0,flag=0,i;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c=c+1;
+        }
+    }
+    if(c==2)
+    {
+        flag=1;
+    }
+    else
+    {
+        flag=0;
+    }
+    return flag;
+}
+int main()
+{
+    int n,i,j,p1,p2,c;
+    scanf("%d",&n);
+    for(i=2;i<=10000;i++)
+    {
+        for(j=2;j<=10000;j++)
+        {
+            if(i!=j && i*j==n)
+            {
+                if(prime(i)==1 && prime(j)==1)
+                {
+              
+                    p1=i;
+                    p2=j;
+                    c=1;
+                    break;
+                    
+                
+            }
+        }
+    }
+    }
+    if(c==1)
+    {
+        printf("%d %d",p2,p1);
+    }
+    else
+    {
+        printf("-1");
+    }
+    
+}
