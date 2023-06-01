bb95a0f352c4337304c953ac7e795c33365aecc5
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-01 19:34:06 +0530

    #include<stdio.h> int fun(int n) {     if (n==0)     {         return 0;     }     else if(n==1)     {         return 1;     }     else if(n==2)     {         return 2;     }     else if(n==3)     {         return 4;     }     else     {         return fun(n-1)+fun(n-2)+fun(n-3);     } } int main() {     int n,res;     scanf("%d",&n);     res=fun(n);     printf("%d",res); }

--- Count_number_of_ways_to_cover_a_distance.c
+++ Count_number_of_ways_to_cover_a_distance.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
-int c(int n)
-{ 
-if(n<=0) 
+int fun(int n)
 {
- return 0; 
-}
- else if(n==1)
- { 
-return 1; 
-} 
-else if(n==2) 
-{ 
-return 2; 
-} 
-else if(n==3)
- {
- return 4; 
-}
- return c(n-1)+c(n-2)+c(n-3);
+    if (n==0)
+    {
+        return 0;
+    }
+    else if(n==1)
+    {
+        return 1;
+    }
+    else if(n==2)
+    {
+        return 2;
+    }
+    else if(n==3)
+    {
+        return 4;
+    }
+    else
+    {
+        return fun(n-1)+fun(n-2)+fun(n-3);
+    }
 }
 int main()
-{ 
-int n; 
-scanf("%d",&n);
- printf("%d",c(n));
+{
+    int n,res;
+    scanf("%d",&n);
+    res=fun(n);
+    printf("%d",res);
 }
