976082cb4357e1b16862c78a5f776054fc68276a
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-01 19:36:58 +0530

    #include<stdio.h> int main() {     int m,n;     scanf("%d%d",&m,&n);     if(m%2==0 || n%2==0)     {         printf("Player 1");     }     else     {         printf("Player 2");     } }

--- Predict_the_winner_in_Coin_Game.c
+++ Predict_the_winner_in_Coin_Game.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 int main()
-{ 
-int a,b,m,n; 
-scanf("%d%d",&m,&n); 
-if(m%2==0 || n%2==0) 
-{ 
-printf("Player 1");
- }
- else
- {
- printf("Player 2"); 
+{
+    int m,n;
+    scanf("%d%d",&m,&n);
+    if(m%2==0 || n%2==0)
+    {
+        printf("Player 1");
+    }
+    else
+    {
+        printf("Player 2");
+    }
 }
-}
-
-
