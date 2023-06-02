04b8670bd5e0004c8882d85b035a129488a52075
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-02 13:47:48 +0530

    #include<stdio.h> int main() {     int n,i,fa,fb,fn,df=0,dl=0;     scanf("%d",&n);     int ar[1000];     fa=0;     fb=1;     for(i=0;i<100;i++)     {         ar[i]=fa;         fn=fa+fb;         fa=fb;         fb=fn;     }     for(i=0;i<100;i++)     {         if(n<ar[i])         {             df=n-ar[i-1];             dl=ar[i]-n;             if(df>dl)             {                 printf("%d",ar[i]);                 break;             }             else if(df==dl)             {                 printf("%d %d",ar[i-1],ar[i]);                 break;             }             else             {                 printf("%d",ar[i-1]);                 break;             }         }     }
    }

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,25 +1,41 @@
 #include<stdio.h>
 int main()
-{ 
-int n,a=0,b=1,c,t;
- scanf("%d",&n);
- c=a+b; 
-t=n; 
-while(c<=t)
- {
- c=a+b; 
-a=b;
- b=c; 
-}
- if(n-a>b-n) 
-{ 
-printf("%d",b);
- } 
-else if(n-a<b-n)
- { 
-printf("%d",a);
- } 
-else { 
-printf("%d %d",a,b);
- } 
+{
+    int n,i,fa,fb,fn,df=0,dl=0;
+    scanf("%d",&n);
+    int ar[1000];
+    fa=0;
+    fb=1;
+    for(i=0;i<100;i++)
+    {
+        ar[i]=fa;
+        fn=fa+fb;
+        fa=fb;
+        fb=fn;
+    }
+    for(i=0;i<100;i++)
+    {
+        if(n<ar[i])
+        {
+            df=n-ar[i-1];
+            dl=ar[i]-n;
+            if(df>dl)
+            {
+                printf("%d",ar[i]);
+                break;
+            }
+            else if(df==dl)
+            {
+                printf("%d %d",ar[i-1],ar[i]);
+                break;
+            }
+            else
+            {
+                printf("%d",ar[i-1]);
+                break;
+            }
+        }
+    }
+    
+    
 }
