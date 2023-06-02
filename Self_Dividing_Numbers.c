eb46a0f7751e02a82de0b37b53fccebdf0b0e0c9
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-02 15:23:03 +0530

    #include<stdio.h> int self(int n) {     int x=n,d=0,r=0,c=0,f=0;     while(n!=0)     {         r=n%10;         d=d+1;         if(r!=0 && x%r==0)         {             c=c+1;
            }
            n=n/10;
            
        }
        if(d==c)
        {
            f=1;
        }
        return f;
    }
    int main()
    {
        int n,m,i;
        scanf("%d%d",&n,&m);
        for(i=n;i<=m;i++)
        {
            if(self(i)==1)
            {
                printf("%d ",i);
            }
        }
        
    }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,22 +1,35 @@
 #include<stdio.h>
+int self(int n)
+{
+    int x=n,d=0,r=0,c=0,f=0;
+    while(n!=0)
+    {
+        r=n%10;
+        d=d+1;
+        if(r!=0 && x%r==0)
+        {
+            c=c+1;
+            
+        }
+        n=n/10;
+        
+    }
+    if(d==c)
+    {
+        f=1;
+    }
+    return f;
+}
 int main()
-{ 
-int n,m,c=0,t=0,i,r,q; 
-scanf("%d%d",&n,&m);
- for(i=n;i<=m;i++)
- { 
-q=i; c=0; t=0;
- while(q!=0)
- { 
-c=c+1; 
-r=q%10; 
-if(r!=0 && i%r==0)
- t=t+1; 
-q=q/10; 
+{
+    int n,m,i;
+    scanf("%d%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+        if(self(i)==1)
+        {
+            printf("%d ",i);
+        }
+    }
+    
 }
- if(c==t)
- {
- printf("%d ",i);
- } 
-}
- }
