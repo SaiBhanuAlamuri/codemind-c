7a15a7562aec8baeb058db7e29cdf37e1570547c
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-02 11:35:21 +0530

    #include<stdio.h> int main() {     int n,m,h,i;     scanf("%d%d",&n,&m);     for(i=1;i<=n;i++)     {         if(n%i==0 && m%i==0)         {             h=i;         }     }     printf("%d",h); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,17 +1,14 @@
 #include<stdio.h>
 int main()
-{ 
-int n,m,h,a;
- scanf("%d%d",&n,&m); 
-while(n!=m)
- { 
-if(n>m) 
 {
- n=n-m; 
-} 
-else {
- m=m-n; 
-} 
-}
- printf("%d",n);
+    int n,m,h,i;
+    scanf("%d%d",&n,&m);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0 && m%i==0)
+        {
+            h=i;
+        }
+    }
+    printf("%d",h);
 }
