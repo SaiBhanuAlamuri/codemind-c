f47974d26f92ff60ce8016def4e3127b136679d1
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-02 12:31:44 +0530

    #include<stdio.h> int pal(int n) {     int r=0,rev=0,x,f=0;     x=n;     while(n!=0)     {         r=n%10;         rev=rev*10+r;         n=n/10;
        }
        if(x==rev)
        {
            f=1;
            
        }
        return f;
    }
    int main()
    {
        int n;
        scanf("%d",&n);
        while(n--)
        {
            int m,res;
            scanf("%d",&m);
            res=pal(m);
            if(res==1)
            {
                printf("True
    ");
            }
            else
            {
                printf("False
    ");
            }
            
            
        }
        
    }

--- Palindrome_Number.c
+++ Palindrome_Number.c
@@ -1,28 +1,43 @@
 #include<stdio.h>
+int pal(int n)
+{
+    int r=0,rev=0,x,f=0;
+    x=n;
+    while(n!=0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+        
+    }
+    if(x==rev)
+    {
+        f=1;
+        
+    }
+    return f;
+}
 int main()
-{ 
-int i,n,m; 
-scanf("%d",&n); 
-for(i=0;i<n;i++) 
-{ 
-scanf("%d",&m);
- int a=m; 
-int s=0; 
-while(m!=0)
- { 
-int h=m%10; 
-s=s*10+h; 
-m=m/10;
- } 
-if(s==a)
- {
- printf("True
+{
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        int m,res;
+        scanf("%d",&m);
+        res=pal(m);
+        if(res==1)
+        {
+            printf("True
 ");
- }
- else
- { 
-printf("False
+        }
+        else
+        {
+            printf("False
 ");
- }
- }
+        }
+        
+        
+    }
+    
 }
