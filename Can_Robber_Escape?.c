f09833b03d0204282765e9b76da34a1573443aef
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-02 16:10:27 +0530

    #include<stdio.h> int main() {     int n,f=0,i;     scanf("%d",&n);     int ar[100];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(i=0;i<n;i++)     {         if(ar[i]<n)         {             f=1;         }         else         {             f=0;             break;         }     }     if(f==1)     {         printf("YES");     }     else     {         printf("NO");     } }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -1,24 +1,31 @@
 #include<stdio.h>
-int main(){
- int i,n,r=0; 
-scanf("%d",&n); 
-int a[n]; 
-for(i=0;i<n;i++) 
-{ 
-scanf("%d",&a[i]); 
-} 
-for(i=0;i<n;i++)
- { 
-if(a[i]%2!=0)
- { r++; 
-} 
-} 
-if(r<=2) 
-{ 
-printf("YES");
- } 
-else 
+int main()
 {
- printf("NO"); 
-} 
+    int n,f=0,i;
+    scanf("%d",&n);
+    int ar[100];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]<n)
+        {
+            f=1;
+        }
+        else
+        {
+            f=0;
+            break;
+        }
+    }
+    if(f==1)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
 }
