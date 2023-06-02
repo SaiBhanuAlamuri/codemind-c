d90a07498905c2a97df2c9b4984d1fd2a65e1fd5
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-02 16:00:53 +0530

    #include<stdio.h> int main() {     int n,m,s;     scanf("%d%d",&n,&m);     if(n%2==0 && (n>0 ||  m%2==0))     {         printf("YES");     }     else     {         printf("NO");     } }

--- Can_share_equally?.c
+++ Can_share_equally?.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main()
-{ int a,b; 
-scanf("%d%d",&a,&b);
- if((a>0 || b%2==0) && a%2==0) 
-{ 
-printf("YES"); 
-} 
-else {
- printf("NO"); 
-}
+{
+    int n,m,s;
+    scanf("%d%d",&n,&m);
+    if(n%2==0 && (n>0 ||  m%2==0))
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
 }
