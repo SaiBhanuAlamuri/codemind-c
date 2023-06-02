463bb5430f951d9a67d2eb31cc9e594d1873e6b2
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-06-02 11:59:57 +0530

    #include<stdio.h> int main() {     int n,temp,i,j,r,c=0,count=0;     scanf("%d",&n);     temp=n;     while(temp>0)     {         c++;         temp=temp/10;     }     int ar[c];     temp=n;   for(i=0;i<c;i++)   {         r=temp%10;         ar[i]=r;         temp=temp/10;   }   for(i=0;i<c;i++)     {         for(j=0;j<c;j++)         {             if(i!=j)             {                 if(ar[i]==ar[j])                 {                     count++;                     break;                 }
                }
            }
        }
        if(count>0)
        {
            printf("Not Unique Number
    ");
        }
        else
        {
            printf("Unique Number
    ");
        }
        
    }

--- Unique_number.c
+++ Unique_number.c
@@ -1,33 +1,46 @@
 #include<stdio.h>
 int main()
-{ 
-int n,num,i,j,r,k=0,flag=1; 
-int arr[100]; 
-scanf("%d",&n); 
-num=n;
- while(n>0)
- { 
-r=n%10; 
-arr[k]=r; 
-n=n/10;
- k++; 
-} 
-for(i=0;i<k;i++)
- {
- for(j=i+1;j<k;j++)
- {
- if(arr[i]==arr[j])
- {
- flag=0;
- }
- }
- }
- if(flag==1)
- { 
-printf("Unique Number");
- } 
-else 
 {
- printf("Not Unique Number");
- } 
+    int n,temp,i,j,r,c=0,count=0;
+    scanf("%d",&n);
+    temp=n;
+    while(temp>0)
+    {
+        c++;
+        temp=temp/10;
+    }
+    int ar[c];
+    temp=n;
+  for(i=0;i<c;i++)
+  {
+        r=temp%10;
+        ar[i]=r;
+        temp=temp/10;
+  }
+  for(i=0;i<c;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i!=j)
+            {
+                if(ar[i]==ar[j])
+                {
+                    count++;
+                    break;
+                }
+                
+            }
+        }
+    }
+    if(count>0)
+    {
+        printf("Not Unique Number
+");
+    }
+    else
+    {
+        printf("Unique Number
+");
+    }
+    
 }
