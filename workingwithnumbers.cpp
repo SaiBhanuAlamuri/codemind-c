246ebaf6a8a9910546986c5f307fd12b238a4fbd
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-07-16 11:58:57 +0530

    Add files via upload

---
+++ workingwithnumbers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	cout<<5+7<<endl;
+	cout<<5-7<<endl;
+	cout<<5/7<<endl;
+	cout<<10%3<<endl;
+	cout<<5+3*9<<"\n";
+	cout<<"Print \n";
+	cout<<10.0/3.0<<endl;
+	cout<<"print 2\n";
+	cout<<10+3.0<<endl;
+	cout<<"Math functions"<<endl;
+	cout<<pow(2,5)<<"\n";
+	cout<<round(4.33)<<"\n";
+	cout<<sqrt(25)<<"\n";
+	cout<<floor(4.8)<<"\n";
+	cout<<fmax(10,20)<<"\n";
+	cout<<fmin(10,20)<<"\n";
+}
